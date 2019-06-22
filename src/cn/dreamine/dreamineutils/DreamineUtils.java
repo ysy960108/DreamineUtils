89df1df04a2f488b77c8862c432a6b920095aaaf
ysy960108 <admin@233233233.com>
2019-06-22 22:42:42 +0200

    Add blank Spigot plugin.

---
+++ src/cn/dreamine/dreamineutils/DreamineUtils.java
@@ -0,0 +1,21 @@
+package cn.dreamine.dreamineutils;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by ysy on 2019/6/22.
+ */
+public class DreamineUtils extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public  void  onDisable(){
+
+    }
+
+}
